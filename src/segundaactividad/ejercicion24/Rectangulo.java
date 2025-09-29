46bffe8bc38875a852aca99aad27e7d23c4694b8
LUISDYG22 <lyepez@unal.edu.co>
2025-09-28 20:12:03 -0500

    Rectangulo.java

--- src/segundaactividad/ejercicion24/Rectangulo.java
+++ src/segundaactividad/ejercicion24/Rectangulo.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ejercicion24;
 
-/**
- *
- * @author Luis Daniel Yepez G
- */
 public class Rectangulo {
     int base;
     int altura;
