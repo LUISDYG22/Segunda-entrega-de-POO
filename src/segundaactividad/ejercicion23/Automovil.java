c7b1d8e678bcacef5e1788b7ee323ec50c811018
LUISDYG22 <lyepez@unal.edu.co>
2025-09-28 20:09:30 -0500

    Automovil.java

--- src/segundaactividad/ejercicion23/Automovil.java
+++ src/segundaactividad/ejercicion23/Automovil.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ejercicion23;
 
-/**
- *
- * @author Luis Daniel Yepez G
- */
 public class Automovil {
     String marca;
     int modelo;
