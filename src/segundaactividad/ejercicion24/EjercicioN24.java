f3d8a80d5d6c15bc9b8347e595ba7f1c2f0b3a61
LUISDYG22 <lyepez@unal.edu.co>
2025-09-28 20:11:42 -0500

    EjercicioN24.java

--- src/segundaactividad/ejercicion24/EjercicioN24.java
+++ src/segundaactividad/ejercicion24/EjercicioN24.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package ejercicion24;
 
-/**
- *
- * @author Luis Daniel Yepez G
- */
 public class EjercicioN24 {
     public static void main(String[] args) {
         Circulo figura1 = new Circulo(2);
